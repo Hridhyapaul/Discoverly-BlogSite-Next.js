b2eb6ff25740b1a6cd52ab8cdb3c3c9a65904b3c
Hridhyapaul <hpdesign2021@gmail.com>
2023-08-31 23:28:03 +0600

    Initial setup and learned how to create component and dynamic route

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, soluta? Vero maiores voluptatem expedita architecto, cupiditate quis laborum, laboriosam, perferendis porro quo in magnam sapiente natus repellat eos! Voluptates, reiciendis? Magnam rem rerum quo fugiat, voluptas corrupti quis consequatur officiis aliquid nemo distinctio impedit, neque id dolorem iure? Esse, explicabo!</h1>
+    </div>
+  )
+}
