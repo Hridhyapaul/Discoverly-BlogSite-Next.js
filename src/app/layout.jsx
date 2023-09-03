b926867a35ead591e18a41220332e12901adf63a
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-04 01:58:43 +0600

    Installed my first next.js project

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,9 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        {children}
-        <Footer></Footer>
+        <div className='content'>
+          <div className='container mx-auto'>
+            <Navbar></Navbar>
+            {children}
+            <Footer></Footer>
+          </div>
+        </div>
       </body>
     </html>
   )
