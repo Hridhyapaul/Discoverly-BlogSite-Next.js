fcd8ae916fdf6b5155f6fe7cbb74c01d093b7462
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-04 02:26:18 +0600

    Created Button component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import hero from '../../public/hero.png'
+import Button from '@/Components/Button/Button'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, soluta? Vero maiores voluptatem expedita architecto, cupiditate quis laborum, laboriosam, perferendis porro quo in magnam sapiente natus repellat eos! Voluptates, reiciendis? Magnam rem rerum quo fugiat, voluptas corrupti quis consequatur officiis aliquid nemo distinctio impedit, neque id dolorem iure? Esse, explicabo!</h1>
-    </div>
+    <>
+    <Button url="/portfolio" text="See Our Works"/>
+    </>
   )
 }
