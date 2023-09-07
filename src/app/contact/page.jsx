cb9dc3597e9ec6d1dab33f3e659a3243bca39984
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-07 21:41:39 +0200

    Created contact form on contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+import Button from '@/Components/Button/Button';
 
 const Contact = () => {
     return (
-        <div>
-            <h1>This is Contact page</h1>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Let's Keep in Touch</h1>
+            <div className={styles.content}>
+                <div className={styles.imgContainer}>
+                    <Image
+                        src='/contact.png'
+                        alt=''
+                        fill={true}
+                        className={styles.image}
+                    >
+                    </Image>
+                </div>
+                <form className={styles.form}>
+                    <input type="text" placeholder='name' className={styles.input} />
+                    <input type="email" placeholder='email' className={styles.input} />
+                    <textarea className={styles.textArea} placeholder='message' cols="30" rows="10"></textarea>
+                    <Button url="/" text="Send"></Button>
+                </form>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      firstFont: "Poppins, sans-serif",
+      secondFont: "Urbanist, sans-serif"
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
