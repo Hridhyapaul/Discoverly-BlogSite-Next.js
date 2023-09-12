80e13690783df058ac68cd2138fa6e79e34cb27e
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-12 09:33:08 -1200

    Implemented styles on blog page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -6,20 +6,36 @@ import Image from 'next/image';
 const Blog = () => {
     return (
         <div className={styles.mainContainer}>
-            <Link href="/testId" className={styles.container}></Link>
-            <div className={styles.imageContainer}>
-                <Image
-                    src="https://images.pexels.com/photos/33152/european-rabbits-bunnies-grass-wildlife.jpg"
-                    alt=""
-                    width={400}
-                    height={250}
-                    className={styles.image}
-                />
-            </div>
-            <div className={styles.content}>
-                <h1 className={styles.title}>Test</h1>
-                <p className={styles.desc}>Description</p>
-            </div>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/33152/european-rabbits-bunnies-grass-wildlife.jpg"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Description</p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/33152/european-rabbits-bunnies-grass-wildlife.jpg"
+                        alt=""
+                        width={400}
+                        height={250}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Description</p>
+                </div>
+            </Link>
         </div>
     );
 };
