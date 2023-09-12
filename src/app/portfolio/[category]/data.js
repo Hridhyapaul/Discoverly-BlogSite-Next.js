9031c358440a645e3795418e0276735a4986e50b
Hridhyapaul <hpdesign2021@gmail.com>
2023-09-12 04:17:21 -1200

    update data.js file

--- src/app/portfolio/[category]/data.js
+++ src/app/portfolio/[category]/data.js
@@ -2,22 +2,22 @@ export const items = {
     applications: [
         {
             id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Creative Portfolio Application",
+            desc: "A beautiful portfolio application to showcase your creative work. It comes with a user-friendly interface and stunning design options.",
             image:
                 "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
         },
         {
             id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Minimal Single Product Application",
+            desc: "A minimalist single product application perfect for showcasing your product in an elegant and simple way.",
             image:
                 "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
         },
         {
             id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Strong Together Charity Application",
+            desc: "A charity application designed to bring people together and support various causes. Make a positive impact on the world with this application.",
             image:
                 "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
         }
@@ -25,22 +25,22 @@ export const items = {
     illustrations: [
         {
             id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Creative Portfolio Illustration",
+            desc: "An artistic illustration representing a creative portfolio. This illustration can be used to enhance the visual appeal of your portfolio website or application.",
             image:
                 "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
         },
         {
             id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Minimal Single Product Illustration",
+            desc: "A minimalist illustration that complements your single product presentation. Use it to convey simplicity and elegance.",
             image:
                 "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
         },
         {
             id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Strong Together Charity Illustration",
+            desc: "An illustration symbolizing unity and support for charitable causes. Use this artwork to inspire and connect with your audience.",
             image:
                 "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
         }
@@ -48,22 +48,22 @@ export const items = {
     websites: [
         {
             id: 1,
-            title: "Creative Portfolio",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Creative Portfolio Website",
+            desc: "A professionally designed website to showcase your creative portfolio. Impress your visitors with stunning visuals and a seamless user experience.",
             image:
                 "https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg",
         },
         {
             id: 2,
-            title: "Minimal Single Product",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Minimal Single Product Website",
+            desc: "An elegant website dedicated to presenting your single product. Capture your audience's attention with simplicity and sophistication.",
             image:
                 "https://images.pexels.com/photos/2103127/pexels-photo-2103127.jpeg",
         },
         {
             id: 3,
-            title: "Strong Together Charity",
-            desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?",
+            title: "Strong Together Charity Website",
+            desc: "A website for promoting and supporting charitable initiatives. Engage your audience in meaningful philanthropic activities with this website.",
             image:
                 "https://images.pexels.com/photos/2916450/pexels-photo-2916450.jpeg",
         }
